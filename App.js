29d85df76c4353379eecd179ef029fbc7d6ac139
marciobecker <ml.becker@hotmail.com>
2023-01-22 18:34:51 -0300

    alteração 2

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Kartenspiel Online Marcio</Text>
+      <Text>Kartenspiel Online</Text>
       <StatusBar style="auto" />
     </View>
   );
