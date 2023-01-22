4091be35d6fe651261a7f1f2e14c64ba2e7a7045
marciobecker <ml.becker@hotmail.com>
2023-01-22 18:29:15 -0300

    altera nome 2

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Kartenspiel Online teste</Text>
+      <Text>Kartenspiel Online Marcio</Text>
       <StatusBar style="auto" />
     </View>
   );
